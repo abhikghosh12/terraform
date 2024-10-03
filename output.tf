698daa5e6d11a147b6bf1650a33fac77fe3afc0a
Abhik Ghosh <abhikghosh87@gmail.com>
2024-10-03 17:22:37 +0200

    added

--- output.tf
+++ output.tf
@@ -6,25 +6,25 @@ output "voice_app_helm_status" {
   value = module.voice_app.helm_status
 }
 
-output "cluster_security_group_id" {
-  description = "Security group ids attached to the cluster control plane"
-  value       = module.eks.cluster_security_group_id
-}
+# output "cluster_security_group_id" {
+#   description = "Security group ids attached to the cluster control plane"
+#   value       = module.eks.cluster_security_group_id
+# }
 
 output "region" {
   description = "AWS region"
   value       = var.aws_region
 }
 
-output "cluster_endpoint" {
-  description = "Endpoint for EKS control plane"
-  value       = module.eks.cluster_endpoint
-}
+# output "cluster_endpoint" {
+#   description = "Endpoint for EKS control plane"
+#   value       = module.eks.cluster_endpoint
+# }
 
-output "cluster_name" {
-  description = "Kubernetes Cluster Name"
-  value       = module.eks.cluster_name
-}
+# output "cluster_name" {
+#   description = "Kubernetes Cluster Name"
+#   value       = module.eks.cluster_name
+# }
 
 # output "kubeconfig_path" {
 #   description = "Path to kubeconfig file"
