9b5c94ec1491884d5500ec74095ffbbfe9ecf9b2
Abhik Ghosh <abhikghosh87@gmail.com>
2024-10-03 17:34:40 +0200

    added

--- modules/voice_app/main.tf
+++ modules/voice_app/main.tf
@@ -114,13 +114,6 @@ data "kubernetes_ingress_v1" "voice_app" {
   depends_on = [helm_release.voice_app]
 }
 
-output "ingress_hostname" {
-  description = "Hostname of the Voice App ingress"
-  value       = try(data.kubernetes_ingress_v1.voice_app.status[0].load_balancer[0].ingress[0].hostname, "")
-}
 
-output "helm_status" {
-  value = helm_release.voice_app.status
-}
 
 
