352c51837cffa613d1f9d988109e47f8a68d7a7d
Abhik Ghosh <abhikghosh87@gmail.com>
2024-10-07 23:45:35 +0200

    added new modules to resolve the problems

--- modules/eks/output.tf
+++ modules/eks/output.tf
@@ -1,23 +1,4 @@
-output "kubeconfig" {
-  value = templatefile("${path.module}/kubeconfig.tpl", {
-    cluster_name = aws_eks_cluster.main.name,
-    endpoint = aws_eks_cluster.main.endpoint,
-    certificate_authority_data = aws_eks_cluster.main.certificate_authority[0].data
-  })
-}
-
-output "node_group_role_name" {
-  value = aws_iam_role.eks_node_group_role.name
-}
-
-output "node_group_role_arn" {
-  value = aws_iam_role.eks_node_group_role.arn
-}
-
-
-output "cluster_security_group_id" {
-  value = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
-}
+# modules/eks/outputs.tf
 
 output "cluster_name" {
   value = aws_eks_cluster.main.name
@@ -29,4 +10,16 @@ output "cluster_endpoint" {
 
 output "cluster_ca_certificate" {
   value = aws_eks_cluster.main.certificate_authority[0].data
+}
+
+output "cluster_security_group_id" {
+  value = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
+}
+
+output "node_group_role_name" {
+  value = aws_iam_role.eks_nodes.name
+}
+
+output "node_group_role_arn" {
+  value = aws_iam_role.eks_nodes.arn
 }
