16505e3586758480b38c1396e2276622621be477
Abhik Ghosh <abhikghosh87@gmail.com>
2024-10-07 23:42:21 +0200

    added new modules to resolve the problems

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -90,18 +90,3 @@ resource "aws_eks_node_group" "main" {
   ]
 }
 
-output "cluster_security_group_id" {
-  value = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
-}
-
-output "cluster_name" {
-  value = aws_eks_cluster.main.name
-}
-
-output "cluster_endpoint" {
-  value = aws_eks_cluster.main.endpoint
-}
-
-output "cluster_ca_certificate" {
-  value = aws_eks_cluster.main.certificate_authority[0].data
-}
--- modules/eks/output.tf
+++ modules/eks/output.tf
@@ -6,8 +6,6 @@ output "kubeconfig" {
   })
 }
 
-
-
 output "node_group_role_name" {
   value = aws_iam_role.eks_node_group_role.name
 }
