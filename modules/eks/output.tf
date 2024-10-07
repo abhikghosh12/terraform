1758339652b28de28565a79f78f59a1543cf185f
Abhik Ghosh <abhikghosh87@gmail.com>
2024-10-07 23:40:43 +0200

    added new modules to resolve the problems

--- modules/eks/output.tf
+++ modules/eks/output.tf
@@ -1,19 +1,3 @@
-output "cluster_name" {
-  value = aws_eks_cluster.main.name
-}
-
-output "cluster_endpoint" {
-  value = aws_eks_cluster.main.endpoint
-}
-
-output "cluster_ca_certificate" {
-  value = aws_eks_cluster.main.certificate_authority[0].data
-}
-
-output "cluster_security_group_id" {
-  value = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
-}
-
 output "kubeconfig" {
   value = templatefile("${path.module}/kubeconfig.tpl", {
     cluster_name = aws_eks_cluster.main.name,
@@ -30,4 +14,21 @@ output "node_group_role_name" {
 
 output "node_group_role_arn" {
   value = aws_iam_role.eks_node_group_role.arn
+}
+
+
+output "cluster_security_group_id" {
+  value = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
+}
+
+output "cluster_name" {
+  value = aws_eks_cluster.main.name
+}
+
+output "cluster_endpoint" {
+  value = aws_eks_cluster.main.endpoint
+}
+
+output "cluster_ca_certificate" {
+  value = aws_eks_cluster.main.certificate_authority[0].data
 }
