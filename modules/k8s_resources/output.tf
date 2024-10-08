600dac668fabc2c9d1bbd0b41d1ef5bee1b5f603
Abhik Ghosh <abhikghosh87@gmail.com>
2024-10-08 12:53:52 +0200

    destroy all

--- modules/k8s_resources/output.tf
+++ modules/k8s_resources/output.tf
@@ -2,6 +2,4 @@ output "storage_class_name" {
   value = kubernetes_storage_class.efs.metadata[0].name
 }
 
-output "pvc_names" {
-  value = [for pvc in kubernetes_persistent_volume_claim.voice_app_pvcs : pvc.metadata[0].name]
-}
+
