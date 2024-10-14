86ab5702357af2aad2ec4d4d7f99563f415336cb
Abhik Ghosh <abhikghosh87@gmail.com>
2024-10-14 15:37:55 +0200

    added pv  size to 5 Gi

--- modules/route53/output.tf
+++ modules/route53/output.tf
@@ -1,17 +1,9 @@
 # modules/route53/outputs.tf
 
-output "certificate_status" {
-  value = aws_acm_certificate.main.status
-}
-
 output "zone_id" {
   value = aws_route53_zone.main.zone_id
 }
 
 output "name_servers" {
   value = aws_route53_zone.main.name_servers
-}
-
-output "certificate_arn" {
-  value = aws_acm_certificate.main.arn
 }
